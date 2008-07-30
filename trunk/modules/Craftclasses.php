c74690739c24a31bec82e85991cfc1c5e20101eb
noerdk <noerdk@06a337a1-f650-0410-a19a-b7d0e371acf7>
2008-07-30 10:01:09 +0000

    Included changes to Gemcutting module done by Varka. Included module to handle resources for city building by madrulf. Included changes to Craftclasses module done by GBH.

--- trunk/modules/Craftclasses.php
+++ trunk/modules/Craftclasses.php
@@ -1,11 +1,13 @@
 <?php
 ///////////////////////////////////
-// craftclasses.php 1.0 for BeBot
+// craftclasses.php 1.1 for BeBot
 ///////////////////////////////////
 // (c) Copyright 2008 by Allan Noer
 // All Rights Reserved
 // Licensed for distribution under the GPL (Gnu General Public License) version 2.0 or later
 ///////////////////////////////////
+// Updated to 1.1 by Buffarse - Added additional parsing and user feedback to !setclass
+//
 
 $craftclasses = new craftclasses($bot);
 
@@ -18,7 +20,7 @@ class craftclasses Extends BaseActiveModule
 
 	var $last_log;
 	var $start;
-
+	
 	// Constructor
 	function __construct (&$bot)
 	{
@@ -50,7 +52,7 @@ class craftclasses Extends BaseActiveModule
 
 		$this -> bot -> core("settings") -> create("Craftclasses", "Remind", TRUE, "Should users level 40+ be reminded to set their craft classes?");
 	}
-
+	
 	function command_handler($name, $msg, $origin)
 	{
 		$output = "";
@@ -59,10 +61,25 @@ class craftclasses Extends BaseActiveModule
 			$options_str = $info[1];
 			$options = array();
 			$options = split(" ", $options_str);
-
+			$craftclass = array("Alchemist", "Architect", "Armorsmith", "Gemcutter", "Weaponsmith");
+			$options[0] = ucwords(strtolower($options[0]));
+			$options[1] = ucwords(strtolower($options[1]));
+			if ( empty($options[0]) || empty($options[1]) )
+			{
+			$output = "You MUST set both craft classes at the same time.";
+			}
+			elseif
+			((array_search ($options[0], $craftclass) !== false) && (array_search ($options[1], $craftclass) !== false))
+			{
 			$this -> bot -> db -> query('INSERT INTO #___craftingclass (name,class1,class2) VALUES("'.$name.'","'.$options[0].'","'.$options[1].'") ON DUPLICATE KEY UPDATE class1=values(class1), class2=values(class2)');
 			$output = "Thank you for updating your crafting information.";
+			}
+			else
+			{
+			$output = "Classes can ONLY be Alchemist, Architect, Armorsmith, Gemcutter and Weaponsmith and you MUST set both at the same time.";
+			}
 		}
+
 		elseif (preg_match("/^craft$/i", $msg, $info))
 		{
 			$lookup = $this -> bot -> db -> select("SELECT * FROM #___craftingclass WHERE name = '" . $name . "'", MYSQL_ASSOC);
@@ -80,30 +97,27 @@ class craftclasses Extends BaseActiveModule
 
 	function buddy($name, $online, $level, $location)
 	{
-		if(0 == $online || 1 == $online)
-		{
-			if ($this -> bot -> core("notify") -> check($name) && $this -> bot -> core("settings") -> get("Craftclasses", "Remind"))
+		if ($this -> bot -> core("notify") -> check($name) && $this -> bot -> core("settings") -> get("Craftclasses", "Remind"))
+		{			
+			$id = $this -> bot -> core("chat") -> get_uid($name);
+			if ($msg == 1)
 			{
-				$id = $this -> bot -> core("chat") -> get_uid($name);
-				if ($online == 1)
+				if ($this -> last_log["on"][$name] < (time() - 5))
 				{
-					if ($this -> last_log["on"][$name] < (time() - 5))
+					$result = $this -> bot -> core("whois") -> lookup($name);
+					if (empty($result["craft1"]) & $result["level"] > 40)
 					{
-						$result = $this -> bot -> core("whois") -> lookup($name);
-						if (empty($result["craft1"]) & $result["level"] > 40)
-						{
-							$msg = "You have no crafting information set and you are above level 40. Please use '!setcraft [class1] [class2]'. Classes can be Alchemist, Architect, Armorsmith, Gemcutter and Weaponsmith. If you havn't picked crafting classes yet this may be the time to do it.";
-							$this -> bot -> send_tell($name, $msg);
-						}
-						$this -> last_log["on"][$name] = time();
+						$msg = "You have no crafting information set and you are above level 40. Please use '!setcraft [class1] [class2]'. Classes can be Alchemist, Architect, Armorsmith, Gemcutter and Weaponsmith. If you havn't picked crafting classes yet this may be the time to do it.";
+						$this -> bot -> send_tell($name, $msg);
 					}
+					$this -> last_log["on"][$name] = time();
 				}
-				else
+			}
+			else
+			{
+				if ($this -> last_log["off"][$name] < (time() - 5))
 				{
-					if ($this -> last_log["off"][$name] < (time() - 5))
-					{
-						$this -> last_log["off"][$name] = time();
-					}
+					$this -> last_log["off"][$name] = time();
 				}
 			}
 		}
